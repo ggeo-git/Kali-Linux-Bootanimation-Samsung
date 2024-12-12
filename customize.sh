6297b73b45048e7d63b9559d04c840d7b2d314c7
ggeo-git <geovannesvff@gmail.com>
2024-12-12 14:33:05 -0300

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+echo "by: ggeo-git"
+sleep 1.0
+echo "module version: 1.0"
+sleep 1.0
+echo "module name: Bootanimation Kali Linux samsung"
+sleep 1.0
+echo "telegram group: ggeo-git-files"
